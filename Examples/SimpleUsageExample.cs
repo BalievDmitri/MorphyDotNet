471db0c0d2f16521a65ab4bddb05047971c028fc
Baliev Dmitri <bal-dim@yandex.ru>
2018-09-09 11:39:25 +0300

    Added a project that will contain interactive usage examples.

---
+++ Examples/SimpleUsageExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MorphyDotNet;
+
+namespace Examples
+{
+    class SimpleUsageExample
+    {
+        public SimpleUsageExample()
+        {
+            MorphAnalyzer morphAnalizer = new MorphAnalyzer("MyDictionaryFolderPath");
+            List<Parse> myWordParses = morphAnalizer.Parse("myWord");
+
+            foreach(var wordParse in myWordParses)
+            {
+                string wordParseStringRepresentation = wordParse.ToString();
+                Tag wordTagWithGrammemes = wordParse.Tag;
+                string wordGrammemesList = wordTagWithGrammemes.ToString();
+            }
+
+        }
+    }
+}
