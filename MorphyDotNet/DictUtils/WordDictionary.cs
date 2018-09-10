6ab20f2721203e72f073b6a383da8c34e0acc53c
Baliev Dmitri <bal-dim@yandex.ru>
2018-09-10 10:09:16 +0300

    Moved paradigm operations into ParadigmsCollection

--- MorphyDotNet/DictUtils/WordDictionary.cs
+++ MorphyDotNet/DictUtils/WordDictionary.cs
@@ -14,13 +14,11 @@ namespace MorphyDotNet.DictUtils
     {
         static readonly NLog.Logger s_logger = NLog.LogManager.GetCurrentClassLogger();
         IWordMatchingDictionary m_wordMatchingDictionary;
-        IParadigmStrings m_tags;
-        List<List<int>> m_paradigms;
+        ParadigmsCollection m_paradigms;
 
-        public WordDictionary(IWordMatchingDictionary dictionary, IParadigmStrings tags, List<List<int>> paradigms)
+        public WordDictionary(IWordMatchingDictionary dictionary, ParadigmsCollection paradigms)
         {
             m_wordMatchingDictionary = dictionary;
-            m_tags = tags;
             m_paradigms = paradigms;
         }
 
@@ -34,16 +32,9 @@ namespace MorphyDotNet.DictUtils
 
             foreach (var pair in pairs)
             {
-                // A paradigm has n entries and n is a multiple of 3
-                // First we have n/3 suffix indexes
-                // Then we have n/3 tag indexes
-                // The we have n/3 prefix indexes
-                // This should probably be later wrapped in a container
-                int n = m_paradigms[pair.ParadigmId].Count();
-                int tagIndex = m_paradigms[pair.ParadigmId][n / 3 + pair.ParadigmIndex];
-                string tag = m_tags.GetString(tagIndex);
+                Tag tag = m_paradigms.GetTag(pair.ParadigmId, pair.ParadigmIndex);
 
-                result.Add(new Parse(word, new Tag(tag)));
+                result.Add(new Parse(word, tag));
             }
 
             return result;
---
+++ MorphyDotNet/Paradigms/ParadigmsCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MorphyDotNet.Paradigms
+{
+    internal sealed class ParadigmsCollection
+    {
+        IParadigmStrings m_tags;
+        List<List<int>> m_paradigms;
+
+        public ParadigmsCollection(List<List<int>> paradigms, IParadigmStrings tags)
+        {
+            m_paradigms = paradigms;
+            m_tags = tags;
+        }
+
+        public Tag GetTag(int paradigmId, int index)
+        {
+            // A paradigm has n entries and n is a multiple of 3
+            // First we have n/3 suffix indexes
+            // Then we have n/3 tag indexes
+            // The we have n/3 prefix indexes
+            // This should probably be later wrapped in a container
+            int n = m_paradigms[paradigmId].Count();
+            int tagIndex = m_paradigms[paradigmId][n / 3 + index];
+            string tag = m_tags.GetString(tagIndex);
+
+            return new Tag(tag);
+        }
+    }
+}
