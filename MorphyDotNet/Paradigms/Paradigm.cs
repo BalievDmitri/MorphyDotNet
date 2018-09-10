d7f07cf3bb598e33a0de9a4f3aa3610d992dd9c5
Baliev Dmitri <bal-dim@yandex.ru>
2018-09-10 10:58:49 +0300

    Created containers for Paradigm and Declension.

---
+++ MorphyDotNet/Paradigms/Paradigm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MorphyDotNet.Paradigms
+{
+    /// <summary>
+    /// This class is a container for paradigm information
+    /// </summary>
+    internal sealed class Paradigm
+    {
+        List<Declension> m_declensions;
+
+        public Paradigm(List<Declension> declensions)
+        {
+            m_declensions = declensions;
+        }
+
+        public Declension GetDeclension(int index)
+        {
+            return m_declensions[index];
+        }
+    }
+}
--- MorphyDotNet/Paradigms/ParadigmsCollection.cs
+++ MorphyDotNet/Paradigms/ParadigmsCollection.cs
@@ -8,26 +8,33 @@ namespace MorphyDotNet.Paradigms
     internal sealed class ParadigmsCollection
     {
         IParadigmStrings m_tags;
-        List<List<int>> m_paradigms;
+        List<Paradigm> m_paradigms;
 
         public ParadigmsCollection(List<List<int>> paradigms, IParadigmStrings tags)
         {
-            m_paradigms = paradigms;
+            m_paradigms = new List<Paradigm>();
+            foreach(var paradigm in paradigms)
+            {
+                var declensions = new List<Declension>();
+                // A paradigm file has n entries and n is a multiple of 3
+                // First we have n/3 suffix indexes
+                // Then we have n/3 tag indexes
+                // The we have n/3 prefix indexes
+                var tagIndexes = paradigm.Skip(paradigm.Count / 3).Take(paradigm.Count / 3);
+
+                foreach (var tagIndex in tagIndexes)
+                {
+                    declensions.Add(new Declension("prefix", new Tag(tags.GetString(tagIndex)), "suffix"));
+                }
+                m_paradigms.Add(new Paradigm(declensions));
+            }
+
             m_tags = tags;
         }
 
         public Tag GetTag(int paradigmId, int index)
         {
-            // A paradigm has n entries and n is a multiple of 3
-            // First we have n/3 suffix indexes
-            // Then we have n/3 tag indexes
-            // The we have n/3 prefix indexes
-            // This should probably be later wrapped in a container
-            int n = m_paradigms[paradigmId].Count();
-            int tagIndex = m_paradigms[paradigmId][n / 3 + index];
-            string tag = m_tags.GetString(tagIndex);
-
-            return new Tag(tag);
+            return m_paradigms[paradigmId].GetDeclension(index).Tag;
         }
     }
 }
